551b1f0bcb846593fd85b6b8f4c43225b6b560d3
Krzysztof Kowalewski <kowalewskikrzysztof144@gmail.com>
2025-07-27 16:51:47 +0200

    Zaktualizowano kod

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import {useEffect, useState } from 'react';
 
 function App() {
   //const boardState = ['','','','','','','','','']
-  const [boardState, changeBoard] = useState(['','','','','','','','',''])
-  const [turn, changeTurn] = useState('X')
+  let [boardState, changeBoard] = useState(['','','','','','','','',''])
+  let [turn, changeTurn] = useState('X')
   const winner = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
   let [whoWon, setWinner] = useState(null);
 
